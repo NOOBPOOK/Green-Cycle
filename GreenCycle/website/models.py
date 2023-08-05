f6bee9c555225652ab8e68e8e7dfc3ae0c482142
Vedant Rajendra Shetye <vedantblackops@gmail.com>
2023-08-05 23:16:28 +0530

    Gmail functionality implemented

--- GreenCycle/website/models.py
+++ GreenCycle/website/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Feedback(models.Model):
+    name = models.CharField(max_length=120)
+    email = models.EmailField(max_length=120)
+    feedback = models.CharField(max_length=200)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
